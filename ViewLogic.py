c4126806010730ff04bdf76dd55238d29caf5159
pablopsp <pablosilio@gmail.com>
2021-03-16 18:24:30 +0100

    Write videofile run in backgroun with QRunnable and temp files support

--- ViewLogic.py
+++ ViewLogic.py
@@ -1,19 +1,26 @@
-from View import *
-from PyQt5 import QtCore, QtWidgets
-import logging
-from threading import Thread
-from moviepy.editor import VideoFileClip, concatenate_videoclips
 import os
+import sys
+import multiprocessing
+import threading
+import logging
 import warnings
 from pathlib import Path
 from itertools import groupby
-import multiprocessing
+
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import QRunnable, QThreadPool
+from threading import Thread
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+from View import *
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
+
+        self.progress_bar.hide()
         
         logTextBox = QTextEditLogger(self)
         logTextBox.setFormatter(logging.Formatter(
@@ -37,6 +44,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.init_video_maker.setDisabled(False)
             logging.info(f"Ruta carpeta de videos: {self.folderPath}")
 
+
     def init_merge(self):
         # First group by name of the .mp4 files
         mp4_paths = [path for path in Path(self.folderPath).rglob('*.mp4') if not 'merged' in path.__str__()]
@@ -52,30 +60,41 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                         path_files[key] = list(group)
                     arranged_files[path] = path_files
 
-
             for path, files_paths in arranged_files.items():
                 result_path = f"{path}\\merged"
                 if not os.path.exists(result_path):
                     os.mkdir(result_path)
 
                 # Concatenate videos by folder dir and file name with 6000 kbps bitrate
-                for keyFile, path_list in files_paths.items():
-                    result_file_path = f"{result_path}\\{keyFile}.mp4"
-
-                    if not os.path.exists(result_file_path):
-                        logging.info(f"Uniendo video: {keyFile}.mp4")
-
-                        videoclips = [VideoFileClip(video.__str__(), audio=False) for video in path_list]
-                        final_clip = concatenate_videoclips(videoclips)
-                        final_clip.write_videofile(result_file_path, bitrate="6000k", threads=multiprocessing.cpu_count(), verbose=False)
-                        
-                        logging.info(f"Video unido en la ruta: {result_file_path}")
-            
+                for key_file, path_list in files_paths.items():
+                    prcs = ProcessRunnable(target=self.concatenate_videos, args=(result_path, key_file, path_list))
+                    prcs.start()
             logging.info("Union de videos finalizada")
-
+        
         else:
             logging.info("No hay ningun video para unir en el directorio actual")
             
+
+    def concatenate_videos(self, result_path, key_file, path_list):
+        temp_result_file_path = f"{result_path}\\{key_file}.temp.mp4"
+        result_file_path = f"{result_path}\\{key_file}.mp4"
+
+        if not os.path.exists(result_file_path):
+            logging.info(f"Uniendo video: {key_file}.mp4")
+
+            videoclips = [VideoFileClip(video.__str__(), audio=False) for video in path_list]
+            final_clip = concatenate_videoclips(videoclips)
+            final_clip.write_videofile(temp_result_file_path, bitrate="6000k", threads=multiprocessing.cpu_count(), verbose=False, codec="libx264")
+            
+            os.rename(temp_result_file_path, result_file_path)
+
+            logging.info(f"Video unido en la ruta: {result_file_path}")
+    
+        elif os.path.exists(temp_result_file_path):
+            os.remove(temp_result_file_path)
+            self.concatenate_videos(result_path, key_file, path_list)
+        
+
 
 class QTextEditLogger(logging.Handler, QtCore.QObject):
     appendPlainText = QtCore.pyqtSignal(str)
@@ -93,9 +112,23 @@ class QTextEditLogger(logging.Handler, QtCore.QObject):
         self.appendPlainText.emit(msg)        
 
 
+
+class ProcessRunnable(QRunnable):
+    def __init__(self, target, args):
+        QRunnable.__init__(self)
+        self.t = target
+        self.args = args
+
+    def run(self):
+        self.t(*self.args)
+
+    def start(self):
+        QThreadPool.globalInstance().start(self)
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     window = MainWindow()
     window.setWindowFlags(QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinimizeButtonHint)
     window.show()
-    app.exec_()
+    sys.exit(app.exec_())
