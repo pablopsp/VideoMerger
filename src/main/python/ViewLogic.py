458b855af53331996170019e6d4c90b9c4009423
pablopsp <pablosilio@gmail.com>
2021-10-11 14:24:53 +0200

    Update requeriments.txt and formatting

--- src/main/python/ViewLogic.py
+++ src/main/python/ViewLogic.py
@@ -1,18 +1,16 @@
 import os
 import sys
 import multiprocessing
-import threading
 import logging
 import warnings
 from pathlib import Path
 from itertools import groupby
 
 import imageio
-imageio.plugins.ffmpeg.download()
 
+imageio.plugins.ffmpeg.download()
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import QRunnable, QThreadPool
-from threading import Thread
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.video.compositing.concatenate import concatenate_videoclips
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
@@ -24,49 +22,57 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
-        
+
         logTextBox = QTextEditLogger(self)
-        logTextBox.setFormatter(logging.Formatter(
-            '%(levelname)s - %(message)s')
+        logTextBox.setFormatter(logging.Formatter("%(levelname)s - %(message)s"))
+        logging.basicConfig(
+            filename="app.log",
+            filemode="a",
+            format="%(name)s - %(levelname)s - %(message)s",
         )
-        logging.basicConfig(filename='app.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s')
         logging.getLogger().addHandler(logTextBox)
         logging.getLogger().setLevel(logging.INFO)
 
         # Ignore moviepy warnings
         warnings.filterwarnings("ignore")
-        
-        self.extensionComboBox.addItems([
-            self.tr('mp4'),
-            self.tr('avi')
-        ])
+
+        self.extensionComboBox.addItems([self.tr("mp4"), self.tr("avi")])
         self.look_for_dir_button.clicked.connect(self.open_file_dialog)
         self.init_video_maker.clicked.connect(self.init_merge)
-    
-    
+
     def open_file_dialog(self):
-        self.folderPath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Seleccione la carpeta que contiene los videos')
-        if(self.folderPath):
+        self.folderPath = QtWidgets.QFileDialog.getExistingDirectory(
+            self, "Seleccione la carpeta que contiene los videos"
+        )
+        if self.folderPath:
             self.dir_name.setText(self.folderPath)
             self.init_video_maker.setDisabled(False)
             logging.info(f"Ruta carpeta de videos: {self.folderPath}")
-
 
     def init_merge(self):
         # Selected extension
         self.selectedExtension = self.extensionComboBox.currentText()
 
         # First group by name of the extension files
-        files_paths = [path for path in Path(self.folderPath).rglob(f"*.{self.selectedExtension}") if not 'merged' in path.__str__()]
+        files_paths = [
+            path
+            for path in Path(self.folderPath).rglob(f"*.{self.selectedExtension}")
+            if not "merged" in path.__str__()
+        ]
 
         if len(files_paths) != 0:
-            group_by_path = [{key: list(group)} for key, group in groupby(files_paths, lambda x: os.path.split(x)[0])]
+            group_by_path = [
+                {key: list(group)}
+                for key, group in groupby(files_paths, lambda x: os.path.split(x)[0])
+            ]
             # Groupby path, file dir and file name
             arranged_files = {}
             for group in group_by_path:
                 for path, values in group.items():
                     path_files = {}
-                    for key, group in groupby(values, lambda x: os.path.split(x)[1].split('-')[0]):
+                    for key, group in groupby(
+                        values, lambda x: os.path.split(x)[1].split("-")[0]
+                    ):
                         path_files[key] = list(group)
                     arranged_files[path] = path_files
 
@@ -80,36 +86,48 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 pool.setMaxThreadCount(1)
 
                 for key_file, path_list in files_paths.items():
-                    prcs = ProcessRunnable(target=self.concatenate_videos, args=(result_path, key_file, path_list))
+                    prcs = ProcessRunnable(
+                        target=self.concatenate_videos,
+                        args=(result_path, key_file, path_list),
+                    )
                     pool.start(prcs)
-        
+
         else:
             logging.info("No hay ningun video para unir en el directorio actual")
-            
 
     def concatenate_videos(self, result_path, key_file, path_list):
-        temp_result_file_path = f"{result_path}\\{key_file}.temp.{self.selectedExtension}"
+        temp_result_file_path = (
+            f"{result_path}\\{key_file}.temp.{self.selectedExtension}"
+        )
         result_file_path = f"{result_path}\\{key_file}.{self.selectedExtension}"
 
         if not os.path.exists(result_file_path):
             logging.info(f"Uniendo video: {key_file}")
 
-            videoclips = [VideoFileClip(video.__str__(), audio=False) for video in path_list]
+            videoclips = [
+                VideoFileClip(video.__str__(), audio=False) for video in path_list
+            ]
             final_clip = concatenate_videoclips(videoclips)
-            final_clip.write_videofile(temp_result_file_path, bitrate="6000k", threads=multiprocessing.cpu_count(), verbose=False, codec="libx264")
-            
+            final_clip.write_videofile(
+                temp_result_file_path,
+                bitrate="6000k",
+                threads=multiprocessing.cpu_count(),
+                verbose=False,
+                codec="libx264",
+            )
+
             os.rename(temp_result_file_path, result_file_path)
 
             logging.info(f"Video unido en la ruta: {result_file_path}")
-    
+
         elif os.path.exists(temp_result_file_path):
             os.remove(temp_result_file_path)
             self.concatenate_videos(result_path, key_file, path_list)
-        
 
 
 class QTextEditLogger(logging.Handler, QtCore.QObject):
     appendPlainText = QtCore.pyqtSignal(str)
+
     def __init__(self, parent):
         super().__init__()
         QtCore.QObject.__init__(self)
@@ -118,10 +136,10 @@ class QTextEditLogger(logging.Handler, QtCore.QObject):
         self.widget.setObjectName("logging_text")
         self.widget.setGeometry(QtCore.QRect(40, 100, 321, 171))
         self.appendPlainText.connect(self.widget.appendPlainText)
-    
+
     def emit(self, record):
         msg = self.format(record)
-        self.appendPlainText.emit(msg)        
+        self.appendPlainText.emit(msg)
 
 
 class ProcessRunnable(QRunnable):
@@ -137,6 +155,8 @@ class ProcessRunnable(QRunnable):
 if __name__ == "__main__":
     appctx = ApplicationContext()
     window = MainWindow()
-    window.setWindowFlags(QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinimizeButtonHint)
+    window.setWindowFlags(
+        QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinimizeButtonHint
+    )
     window.show()
     sys.exit(appctx.app.exec_())
